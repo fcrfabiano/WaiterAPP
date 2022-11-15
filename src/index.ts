030f9be93d7a9fb3eb47104bb963883f1b42782a
Fabiano César <fcfabiano.cesar@gmail.com>
2022-11-15 15:37:14 -0300

    feat(app): initialize mongoose connection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
-const port = 3001;
-app.listen(3001, () => {
-  console.log(`Server is running on http://localhost:${port}/`);
-});
+mongoose.connect(
+  'mongodb+srv://test:test@cluster0.pn5i2xu.mongodb.net/?retryWrites=true&w=majority'
+).then(() => {
+  const app = express();
+  const port = 3001;
+  
+  app.listen(3001, () => {
+    console.log(`Server is running on http://localhost:${port}/`);
+  });
+})
+  .catch((err) => console.log(err));
