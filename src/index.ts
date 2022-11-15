e6ca93f63f248178e4744c1769cceda7fcabb73b
Fabiano César <fcfabiano.cesar@gmail.com>
2022-11-15 19:56:40 -0300

    feat(app): use handleErrors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import 'express-async-errors';
 
 import { router } from './router';
+import { handleErrors } from './app/middlewares/handleErrors';
 
 mongoose.connect(
   'mongodb+srv://test:test@cluster0.pn5i2xu.mongodb.net/?retryWrites=true&w=majority'
@@ -10,6 +12,7 @@ mongoose.connect(
   const port = 3001;
   app.use(express.json());
   app.use(router);
+  app.use(handleErrors);
   app.listen(3001, () => {
     console.log(`Server is running on http://localhost:${port}/`);
   });
