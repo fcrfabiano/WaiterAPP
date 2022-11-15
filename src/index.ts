a2c37591eb414473b0cc15e664cbabf622fa4279
Fabiano César <fcfabiano.cesar@gmail.com>
2022-11-15 15:24:06 -0300

    build(dependecy): add mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express();
-
+const port = 3001;
 app.listen(3001, () => {
-  console.log('Server is running on http://localhost:3001/');
+  console.log(`Server is running on http://localhost:${port}/`);
 });
