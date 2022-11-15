349e324df5e1862a254bbff2f862d61804ec942c
Fabiano César <fcfabiano.cesar@gmail.com>
2022-11-15 15:43:06 -0300

    feat(model): create Product model

--- src/app/models/Category.ts
+++ src/app/models/Category.ts
@@ -1,7 +1,16 @@
 import { model, Schema } from 'mongoose';
 
-export const Category = model('Category', new Schema({
-  name: String,
-  icon: String,
-}));
+export const Category = model(
+  'Category',
+  new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    icon: {
+      type: String,
+      required: true,
+    },
+  })
+);
 
