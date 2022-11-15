cd7c51c1c2950a31791939e9a6fdde9b3fc9212e
Fabiano César <fcfabiano.cesar@gmail.com>
2022-11-15 19:44:49 -0300

    feat(app): implement use express.json() and router

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import { router } from './router';
+
 mongoose.connect(
   'mongodb+srv://test:test@cluster0.pn5i2xu.mongodb.net/?retryWrites=true&w=majority'
 ).then(() => {
   const app = express();
   const port = 3001;
-  
+  app.use(express.json());
+  app.use(router);
   app.listen(3001, () => {
     console.log(`Server is running on http://localhost:${port}/`);
   });
 })
   .catch((err) => console.log(err));
+  
