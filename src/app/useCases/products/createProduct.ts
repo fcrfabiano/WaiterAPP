0d05134e984a1c7c59681929b8ee90e297fa7e1f
Fabiano César <fcfabiano.cesar@gmail.com>
2022-11-15 21:00:41 -0300

    feat(createProduct): implement createProduct

---
+++ src/app/useCases/products/createProduct.ts
@@ -0,0 +1,23 @@
+import { Product } from '../../models/Product';
+import { CreateProductDTO } from './DTO/CreateProductDTO';
+
+
+export async function createProduct({
+  name,
+  description,
+  imagePath,
+  price,
+  ingredients,
+  category,
+}: CreateProductDTO) {
+  const product = await Product.create({
+    name,
+    description,
+    imagePath,
+    price,
+    ingredients: JSON.parse(ingredients),
+    category,
+  });
+
+  return product;
+}
