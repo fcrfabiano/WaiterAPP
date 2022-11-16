8c809d5cc459a83375e3610a30e61324a464c612
Fabiano César <fcfabiano.cesar@gmail.com>
2022-11-15 22:01:56 -0300

    fix(handleError): add next to parameter

--- src/app/middlewares/handleErrors.ts
+++ src/app/middlewares/handleErrors.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { AppError } from '../errors/AppError';
 
-export function handleErrors(err: Error, _req: Request, res: Response) {
+export function handleErrors(err: Error, _req: Request, res: Response, _next: NextFunction) {
   if (err instanceof AppError) return res.status(err.code).json({ message: err.message });
 
   return res.status(500).json({ message: `Internal Server Error: ${err.message}` });
